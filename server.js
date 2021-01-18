2709a3fb1b78810372aecee1667cefe9e957d424
T Schway <tony.schwebach@gmail.com>
2021-01-18 11:08:33 -0500

    added middleware to access css and js files

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ var PORT = process.env.PORT || 8080;
 // middleware - set up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));  // set public as root so css and js assets can be applied to html files
 
 // routes for server created assigned to app variable
 require("./routes/htmlRoutes")(app);
