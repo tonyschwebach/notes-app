71ba607dfdc8a04d9264760da2e40537b202fa25
T Schway <tony.schwebach@gmail.com>
2021-01-18 12:24:31 -0500

    added id when saving notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,6 +2,10 @@
 const path = require("path");
 const db = require("../db/db.json");
 
+// require universal unique id to create unique id for each note
+const {v4:uuidv4} = require('uuid');
+
+
 // export routes for app parameter passed
 module.exports = function (app) {
   // gets notes from db and returns as json
@@ -9,10 +13,20 @@ module.exports = function (app) {
     res.json(db);
   });
 
-    // saves notes to db 
-    app.post("/api/notes", function (req, res) {
-      db.push(req.body)
-      res.json(db);
-    });
-
+  // view specific note
+  app.get("/api/notes/:id", function (req, res) {
+    let chosen = req.params.id;
+    console.log(chosen);
+    for (let i = 0; i < db.length; i++) {
+      if (chosen === db[i].id) {
+        return res.json(db[i]);
+      }
+    }
+  });
+  // saves notes to db
+  app.post("/api/notes", function (req, res) {
+    req.body["id"]=uuidv4();
+    db.push(req.body);
+    res.json(db);
+  });
 };
