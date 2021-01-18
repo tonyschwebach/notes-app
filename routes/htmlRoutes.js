dff089f760826d25cf88d4b254273a24fa4809eb
T Schway <tony.schwebach@gmail.com>
2021-01-18 10:07:52 -0500

    added notes route, correct css link for notes page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -4,6 +4,11 @@ const path = require('path');
 // export routes for app parameter passed
 module.exports = function(app) {
 
+    // go to notes page
+    app.get("/notes", function(req, res) {
+      res.sendFile(path.join(__dirname, "../public/notes.html"));
+    });
+
   // default to index.html homepage
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
